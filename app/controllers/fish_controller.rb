18be3e8e3caf3b936efa7a481933a0574cc6416a
dfert1987 <github email address>
2020-07-27 10:41:16 -0600

    update method added to fish controller. Id also added to fish params

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -7,9 +7,14 @@ class FishController < ApplicationController
     def show
         @fish = Fish.find(params[:id])
     end
-
+    
+    def update
+        @fish = Fish.update(fish_params)
+        render json: @fish
+    end
+    
     private
     def fish_params
-        params.require(:fish).permit(:species, :description, :caught, :image, :pr )
+        params.require(:fish).permit(:species, :description, :caught, :image, :pr, :id )
     end
 end
